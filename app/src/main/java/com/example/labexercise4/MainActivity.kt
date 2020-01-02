41429775b677118b1180e1b313aff501113e7a0d
Gaabu <junjet99@gmail.com>
2020-01-02 21:23:44 +0800

    exercise4

---
+++ app/src/main/java/com/example/labexercise4/MainActivity.kt
@@ -0,0 +1,56 @@
+package com.example.labexercise4
+
+import android.annotation.SuppressLint
+import android.app.DatePickerDialog
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        dobSelected.setOnClickListener { calInvestment() }
+    }
+
+    @SuppressLint("SetTextI18n")
+    private fun calInvestment(){
+        var saving:Double = 0.0
+        var investment:Double
+
+        val date = Calendar.getInstance()
+        val nYear = date.get(Calendar.YEAR)
+        val nMonth = date.get(Calendar.YEAR)
+        val nDay = date.get(Calendar.YEAR)
+
+        val datePd = DatePickerDialog(this, DatePickerDialog.OnDateSetListener{ view, year, month, day ->
+            dobSelected.text = "$day-$month-$year"
+            val CalAge = nYear- year
+            saving = when(CalAge){
+                in 0..15 -> 0.0
+                in 16..20 -> 5000.0
+                in 21..25 -> 14000.0
+                in 26..30 -> 29000.0
+                in 31..35 -> 50000.0
+                in 36..40 -> 78000.0
+                in 41..45 -> 116000.0
+                in 46..50 -> 165000.0
+                in 51..55 -> 228000.0
+                else -> 228000.0
+
+            }
+
+            investment = saving * 0.30
+            age_txt.text = CalAge.toString()
+            savingAmount.text = saving.toString()
+            totalInvestment.text = investment.toString()
+
+
+        }, nYear,nMonth,nDay)
+                datePd.show()
+    }
+}
